edca545580b6d7072a3b07b62ac24029f66c4940
dglzs <dglzs@qq.com>
2015-11-02 15:51:15 +0800

    modify directive

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
 var footer = angular.module('footer', []);
 
-footer.controller('footerController', function() {
-  footer = this;
-  footer.info = {
+footer.controller('footerController', ['$scope', function($scope) {
+  $scope.demostar = {
+    name: 'Demostar',
     version: '0.0.1',
-    status: 'building'
+    status: 'building',
+    year: '2002-2015',
+    website: 'http://www.demostar.com',
+    websiteName: 'website'
   };
-});
+  $scope.beibi = {
+    name: 'Beibi',
+    version: '0.1.4',
+    status: 'building',
+    year: '2012-2015',
+    website: 'http://www.beibi.cn',
+    websiteName: 'our site'
+  };
+  $scope.qiming = {
+    name: '启明星辰',
+    version: '10.0.13980.1',
+    status: 'alpha',
+    year: '2015',
+    website: 'http://alpha.nj.trantect.com/',
+    websiteName: '云子可信官网'
+  }
 
-footer.directive('footerInfo', function() {
+}]);
+
+footer.directive('cfooter', function() {
   return {
     restrict: 'E',
-    templateUrl: './views/footerInfo.html'
-
+    scope: {
+      companyInfo: '=info'
+    },
+    templateUrl: 'views/footerInfo.html'
   };
 });
